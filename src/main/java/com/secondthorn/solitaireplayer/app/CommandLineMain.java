cf05e69ed799fa3b8a1d9fd9b5729225494b47be
Mitchell Chung <mchung94@gmail.com>
2017-05-25 11:37:42 -0700

    Add gradle wrapper, libs, main class

---
+++ src/main/java/com/secondthorn/solitaireplayer/app/CommandLineMain.java
@@ -0,0 +1,7 @@
+package com.secondthorn.solitaireplayer.app;
+
+public class CommandLineMain {
+    public static void main(String[] args) {
+        System.out.println("Nothing's here yet!");
+    }
+}
